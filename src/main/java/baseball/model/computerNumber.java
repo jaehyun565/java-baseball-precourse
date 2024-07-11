9217bb8cce321ba187a2f7774d9261a57c2be7c8
jaehyun <wogus565@gmail.com>
2024-07-11 16:20:23 +0900

    :art: feat: computerNumber 초기화 메서드 추가
    computerNum이 생성될 때 실행되는 clearNum 메서드 추가
    clearNum : computerNum 배열의 모든 수를 0으로 초기화시킴

--- src/main/java/baseball/model/computerNumber.java
+++ src/main/java/baseball/model/computerNumber.java
@@ -2,13 +2,24 @@ package baseball.model;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+// 1. 컴퓨터 3자리 랜덤 숫자 생성
 public class computerNumber {
 
-	// 1. 컴퓨터 3자리 랜덤 숫자 생성
-	public int[] generateComputerNum() {
+	private final int NUMSIZE = 3;
 
-		int[] computerNum = new int[3];
+	int[] computerNum = new int[NUMSIZE];
 
+	private computerNumber() {
+		clearNum();
+	}
+
+	public void clearNum() {
+		for (int i = 0; i < computerNum.length; i++) {
+			computerNum[i] = 0;
+		}
+	}
+
+	public void generateComputerNum() {
 		for (int i = 0; i < 3; i++) {
 			int randNum = Randoms.pickNumberInRange(1, 9);
 			while (computerNum[0] == randNum || computerNum[1] == randNum) {
@@ -16,7 +27,6 @@ public class computerNumber {
 			}
 			computerNum[i] = randNum;
 		}
-		return computerNum;
 	}
 
 }
