2313bf6b6aabddb2036d2ab320143fef41ce911f
jaehyun <wogus565@gmail.com>
2024-07-11 17:47:52 +0900

    :art: feat: 플레이어 입력을 검증하는 기능 구현
    Player가 입력을 하면 해당 입력이 정상적인 입력인이 3가지의 Process를 통해 검증한다 > 시간이 없어 하나의 메서드로 만드는중, 만든 후에 각 기능을 분리시킬 예정

--- src/main/java/baseball/model/PlayerNumber.java
+++ src/main/java/baseball/model/PlayerNumber.java
@@ -12,9 +12,33 @@ public class PlayerNumber {
 		String tmp = Console.readLine();
 
 		for (int i = 0; i < playerNum.length; i++) {
-			playerNum[i] = tmp.charAt(i)-48;
+			playerNum[i] = tmp.charAt(i) - 48;
 			System.out.println(playerNum[i]);
 		}
+	}
+
+	private void validTest(String number) {
+		int testNum = 0;
+		// 입력값이 Int로 만들 수 없으면 error
+		try {
+			testNum = Integer.parseInt(number);
+		} catch (IllegalArgumentException e) {
+			System.err.println("입력은 숫자값으로 입력하셔야 합니다.");
+			return;
+		}
+
+		// 자릿수에 맞게 입력했는지 검증
+		if (number.length() != NUMSIZE) {
+			throw new IllegalArgumentException("입력값이 "+NUMSIZE+"자리가 아닙니다.");
+		}
+
+		// 중복문자 있는지 검증
+//		for (int i = 0; i < number.length(); i++) {
+//			char testWord = number.charAt(i);
+//			if(number.contains()){
+//
+//			}
+//		}
 
 	}
 
