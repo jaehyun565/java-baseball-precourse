28a5e9e92dc0f2691bb3f7e58c47a6d940a701cd
jaehyun <wogus565@gmail.com>
2024-07-11 12:11:16 +0900

    :sparkles: feat: 1.3자리 수 랜덤 난수 생성 구현
    makeRandNum()메서드를 만들어서 서로 다른 3자리 난수를 생성한다.

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,27 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
+    private static int makeRandNum(){
+        int randNum = Randoms.pickNumberInRange(0,999);
+        boolean check = true;
+        while(check){
+            if (randNum < 100){
+                randNum = Randoms.pickNumberInRange(0,999);
+            } else if(randNum/100 == (randNum%100)/10 || randNum/100 == randNum%10 || (randNum%100)/10 == randNum%10){
+                randNum = Randoms.pickNumberInRange(0,999);
+            } else{
+                check = false;
+            }
+        }
+        return randNum;
+    }
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+        // 1. 3자리수 난수 생성 구현
+        int randNum = makeRandNum();
 
+        System.out.println(randNum);
     }
 }
